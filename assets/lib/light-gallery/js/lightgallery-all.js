bc4e45d100c0ff760969277e5956903027102ac6
unknown <han.zenan@yahoo.com>
2021-04-29 10:03:38 +0800

    Initial changes to lightgallery js, css

--- assets/lib/light-gallery/js/lightgallery-all.js
+++ assets/lib/light-gallery/js/lightgallery-all.js
@@ -284,7 +284,6 @@
 		template = '<div class="lg-outer ' + this.s.addClass + ' ' + this.s.startClass + '">' +
 		'<div class="lg" style="width:' + this.s.width + '; height:' + this.s.height + '">' +
 		'<div class="lg-inner">' + list + '</div>' +
-		'<div class="lg-toolbar group">' +
 		'<span class="lg-close lg-icon"></span>' +
 		'</div>' +
 		controls +
@@ -650,7 +649,8 @@
 				_this.$slide.eq(index).prepend('<div class="lg-video-cont "><div class="lg-video"></div></div>');
 				_this.$el.trigger('hasVideo.lg', [index, _src, _html]);
 			} else {
-				_this.$slide.eq(index).prepend('<div class="lg-img-wrap"><img class="lg-object lg-image" src="' + _src + '" /></div>');
+				// _this.$slide.eq(index).prepend('<div class="lg-img-wrap"><img class="lg-object lg-image" src="' + _src + '" /></div>');
+				_this.$slide.eq(index).prepend('<div class="lg-img-wrap"><div class="module-parent"><div class="module-content"><iframe class="module-iframe" src="' + this.s.moduleLearnerURL + '"></iframe>' + '<div class="module-description"><div><h3 class="module-title">' + this.s.moduleTitle + '</h3><p>' + this.s.moduleContent + '</p></div>' + '<a href="' + this.s.moduleEditorURL + '" target="_parent" class="button module-editor-button">Use Template</a></div>' + '</div></div></div>');
 			}
 
 			_this.$el.trigger('onAferAppendSlide.lg', [index]);
